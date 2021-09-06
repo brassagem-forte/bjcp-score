adc21555c0d7be0fe7b9c01d7666087acb7c4f7f
Henrique Boaventura <hboaventura@gmail.com>
2021-09-06 13:27:54 -0300

    fixing layout issue

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -10,7 +10,7 @@
                 <h3 class="font-semibold text-xl text-gray-800 leading-tight">Usuários</h3>
                 <ol class="list-decimal list-inside">
                     @foreach($users as $i => $user)
-                    <li class="text-{{ 4-$i > 1 ? 4-$i : '' }}xl border-b border-gray-200 p-2">
+                    <li class="text-{{ 3-$i > 1 ? 3-$i : '' }}xl border-b border-gray-200 p-2">
                         <a href="{{ route('show', [$user->id, Str::slug($user->name)]) }}">
                         {{ $user->name }}: <span class="md:float-right">{{ $user->total }}</span>
                         </a>
@@ -23,8 +23,6 @@
                 <div class="chart-container" style="position: relative; height:130vh; width:100%">
                     <canvas id="chart"></canvas>
                 </div>
-
-
             </div>
         </div>
     </div>
